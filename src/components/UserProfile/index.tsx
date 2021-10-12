9b1ffb48ddead57c078718dd9adf79ed4f354273
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2021-10-12 18:14:33 -0400

    fix(ui): hide 'errored' sliders too

--- src/components/UserProfile/index.tsx
+++ src/components/UserProfile/index.tsx
@@ -258,7 +258,8 @@ const UserProfile: React.FC = () => {
           [Permission.MANAGE_REQUESTS, Permission.REQUEST_VIEW],
           { type: 'or' }
         )) &&
-        (!requests || !!requests.results.length) && (
+        (!requests || !!requests.results.length) &&
+        !requestError && (
           <>
             <div className="slider-header">
               <Link
@@ -276,10 +277,8 @@ const UserProfile: React.FC = () => {
             </div>
             <Slider
               sliderKey="requests"
-              isLoading={!requests && !requestError}
-              isEmpty={
-                !!requests && !requestError && requests.results.length === 0
-              }
+              isLoading={!requests}
+              isEmpty={!!requests && requests.results.length === 0}
               items={(requests?.results ?? []).map((request) => (
                 <RequestCard
                   key={`request-slider-item-${request.id}`}
