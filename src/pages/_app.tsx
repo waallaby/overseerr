3c58169285a364b450923dc8ce20641fd0c64e65
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2022-01-25 08:03:49 -0500

    fix: verify Plex server access on local logins as well

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -160,7 +160,7 @@ CoreApp.getInitialProps = async (initialProps) => {
     hideAvailable: false,
     movie4kEnabled: false,
     series4kEnabled: false,
-    localLogin: false,
+    localLogin: true,
     region: '',
     originalLanguage: '',
     partialRequestsEnabled: true,
